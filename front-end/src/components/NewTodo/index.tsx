0a3ab72d30adf5ebc79389b00f624dbac1094a3a
Mary Skywalker <snowdennaruto90@gmail.com>
2022-01-06 20:35:13 -0400

    wip

--- front-end/src/components/NewTodo/index.tsx
+++ front-end/src/components/NewTodo/index.tsx
@@ -1,10 +1,40 @@
 import * as C from './style';
+import api from '../../services/api';
+import { FormEvent, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 export const NewTodo = () => {
+    const navigate = useNavigate();
+    const [ description, setDescription ] = useState('');
+
+    async function handleSubmit(event: React.FormEvent<HTMLFormElement>){
+        try {
+            event.preventDefault();
+
+            const data = new FormData();
+
+            data.append('description', description);
+
+            await api.post('todo', data);
+            console.log("data", data);
+
+            navigate('/');
+        } catch (error){
+            alert('Erro interno, tente novamente mais tarde! ' + error);
+        }
+    }
+
     return(
         <C.container>
-            <form action="">
-                <input type="text" placeholder='Digite'/>
+            <form onSubmit={handleSubmit}>
+                <input
+                 type="text"
+                 name="text"
+                 id="text"
+                 placeholder='Digite' 
+                 onChange={event => setDescription(event.target.value)} 
+                 value={description}
+                />
             </form>
         </C.container>
     );
