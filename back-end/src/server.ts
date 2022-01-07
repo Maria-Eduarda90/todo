8a72ec510a3fb01303d27022e3de773c401451a7
Mary Skywalker <snowdennaruto90@gmail.com>
2022-01-06 20:39:26 -0400

    wip

--- back-end/src/server.ts
+++ back-end/src/server.ts
@@ -1,16 +1,15 @@
 import "reflect-metadata";
 import express, { NextFunction, Request, Response } from 'express';
 import 'express-async-errors';
-
-import { router } from './routes/routes';
-
 import './database';
+import cors from 'cors';
+import { router } from './routes/routes';
 import { ErrorsUser } from "./errors/handle";
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
-
 app.use(router);
 
 app.use((err: Error, request: Request, response: Response, _next: NextFunction) => {
@@ -26,7 +25,7 @@ app.use((err: Error, request: Request, response: Response, _next: NextFunction) 
     });
 })
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3333;
 
 app.listen(port, () => {
     console.log(`rodando na pota ${port}`)
