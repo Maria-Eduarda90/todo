155a7e478a5e24cc01496a14fa7800a3903be162
Mary Skywalker <snowdennaruto90@gmail.com>
2022-01-06 20:34:36 -0400

    wip

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,26 +1,6 @@
-import { ThemeProvider } from 'styled-components';
-import { Header } from './components/Header';
-import { NewTodo } from './components/NewTodo';
-import { Todo } from './components/Todo';
-import GlobalStyle from './styles/global';
-import light from './styles/theme/light';
-import dark from './styles/theme/dark';
-import { usePersistedState } from './utils/usePersistedState';
-
+import { Rotas } from './routes';
 export const App = () => {
-  const [ theme, setTheme ] = usePersistedState('theme', light);
-
-  const toggleTheme = () => {
-    setTheme(theme.title === 'light' ? dark : light);
-  }
   return (
-    <>
-    <ThemeProvider theme={theme}>
-     <GlobalStyle/>
-     <Header toggleTheme={toggleTheme}/>
-     <NewTodo/>
-     <Todo/>
-    </ThemeProvider>
-    </>
+    <Rotas/>
   );
 }
