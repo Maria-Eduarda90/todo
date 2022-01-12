6ac6f3ff3074056076f603d85892380c124a9d29
Mary Skywalker <snowdennaruto90@gmail.com>
2022-01-12 11:44:03 -0400

    style

--- front-end/src/components/Todo/style.ts
+++ front-end/src/components/Todo/style.ts
@@ -8,14 +8,16 @@ export const container = styled.div `
     width: 50vw;
     margin-left: auto;
     margin-right: auto;
+    margin-bottom: 1rem;
     .container {
         display: flex;
         justify-content: space-between;
-        border: 1px solid #131010;
+        border: 1px solid ${props => props.theme.colors.background};;
         background: ${props => props.theme.colors.secundary};
+        box-shadow: 0.25px 0.25rem 0.25rem 0.25rem rgba(17, 76, 116, 0.2);
         padding: 1rem;
         border-radius: 0.25rem;
-        
+        margin-bottom: 0.20rem;
     }
     .container button {
         background: none;
