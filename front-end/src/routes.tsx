abfb9e5abfc619bbdb48eaff0ef1e333c25e89bb
Mary Skywalker <snowdennaruto90@gmail.com>
2022-01-12 16:06:32 -0400

    wip

--- front-end/src/routes.tsx
+++ front-end/src/routes.tsx
@@ -1,4 +1,4 @@
-import {BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import {BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import {TodoPage} from './pages/todo';
 
 export function Rotas(){
